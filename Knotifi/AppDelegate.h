94bcbdc41e5a6da7bb63b3e1bb100d1ed82c737c
Sidd <sidd@Sidds-MacBook-Air.local>
2013-12-18 16:58:49 -0800

    Initial Commit

---
+++ Knotifi/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Knotifi
+//
+//  Created by Sidd on 12/18/13.
+//  Copyright (c) 2013 Knotifi, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
