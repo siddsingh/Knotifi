93d9067f7d768661c61f2ab183e4bebc09a66e61
Sidd Singh <sidd.singh@gmail.com>
2014-01-02 18:40:14 -0800

    Started basic UI for List View

---
+++ Knotifi/KNTasksViewController.h
@@ -0,0 +1,15 @@
+//
+//  KNTasksViewController.h
+//  Knotifi
+//
+//  Class that manages the view showing task list and navigation options.
+//
+//  Created by Sidd on 1/2/14.
+//  Copyright (c) 2014 Knotifi, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KNTasksViewController : UIViewController
+
+@end
---
+++ Knotifi/KNTasksViewController.m
@@ -0,0 +1,40 @@
+//
+//  KNTasksViewController.m
+//  Knotifi
+//
+//  Class that manages the view showing task list and navigation options.
+//
+//  Created by Sidd on 1/2/14.
+//  Copyright (c) 2014 Knotifi, Inc. All rights reserved.
+//
+
+#import "KNTasksViewController.h"
+
+@interface KNTasksViewController ()
+
+@end
+
+@implementation KNTasksViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
